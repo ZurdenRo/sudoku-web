1df4e643e77c132f05c72530244d5d8a0f37d0d6
ZurdenRo <rodrigo.nievarobles@gmail.com>
2022-03-08 22:53:25 -0300

    modify project

--- pages/about.js
+++ pages/about.js
@@ -2,25 +2,14 @@ import React, {useState} from 'react'
 
 const About = () => {
 
-    const [obj, sarasa] = useState({number : 1, name: 'ro', lastName: 'last'})
-   
-    function decrementCount(){
-        sarasa( state => ({number: state.number - 1}) )
-    }
-
-    function incrementCount(){
-        sarasa( state => ({number: state.number + 1}))
-    }
-
     return (
         <>
-            <button onClick={decrementCount}>-</button>
-            <span>{obj.number}</span> 
-            <button onClick={incrementCount}>+</button>
-            <p></p>
-            <span>{obj.name}</span>
+         
         </>
     );
+   
 }
+
+
 
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ import React, {useState} from 'react'
 
 
 export default function Home(){
-    const [obj, setGrid] = useState({grid: ''});
+   
+    const [checked, isChecked] = useState(false)
 
-    const functionClick = () => {
-        setGrid(obj => ({grid : obj.value}))
-        console.log(obj.grid)
+    const functionCheck = (e) => {
+        console.log(e.checked)
     }
 
     return(
@@ -22,20 +22,9 @@ export default function Home(){
                 <meta name='keyboards' content='mypage'/>
             </Head>
             <main className={styles.main}>
-               <div className='container'>  
-                    <div className='select-grid'>
-                        <form>
-                            <input type='radio' name='grid' value='2' onClick={functionClick}></input>
-                            <label>Grid 2x2</label>
-                            <br/>
-                            <input type='radio' name='grid' value='3' onChange={functionClick}></input>
-                            <label>Grid 3x3</label>
-                            <br/>
-                        </form>
-                        <button>
-                            <p>Start</p>
-                        </button>
-                    </div> 
+               
+               <div>
+                    <input type='checkbox' name='checked' value={checked} onChange={functionCheck}></input>
                </div>
             
             </main>
