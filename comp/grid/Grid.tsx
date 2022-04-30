46b1198c5960aa2a819464aabc028cb9f00c4637
Rodrigo Nieva <rodrigo.nieva@rodri-mac.local>
2022-04-30 17:24:58 -0300

    other lines to migrate ts

--- comp/grid/Grid.tsx
+++ comp/grid/Grid.tsx
@@ -1,23 +1,118 @@
 import React, {useState, useEffect} from 'react'
 import json from '../../resources/data.json'
 
+interface Cell {
+    num : number,
+    hidden: boolean,
+    position: Position
+}
+
+interface Position {
+    posX: number,
+    posY: number
+}
+
+interface PositionGrid{
+    cellsMatrix: Cell[][]
+    idGrid: number
+}
+
+interface Table{
+
+}
+
+interface TableFrontUser{
+
+}
 
 function GenerateGrid({data, showForm}: {data: PropGame, showForm: () => void}){
 
-    const [table, setTable] = useState()
-    const [tableToMatch, setTableToMatch] = useState()
-    const [isEqual, setEqual] = useState(false)
+    const [table, setTable] = useState<Table>()
+    const [tableToMatch, setTableToMatch] = useState<TableFrontUser>()
+    const [isEqual, setEqual] = useState<Boolean>(false)
     
+    const getRandomArbitrary = (num: number): Position[] => {
+        let randomTmp : Position [] = [];
+        while(randomTmp.length < num){
+            let posRandom : Position = {
+                posX: Math.floor(Math.random() * num),
+                posY: Math.floor(Math.random() * num)
+            }
+            if (randomTmp.length === 0) {
+                randomTmp.push(posRandom);
+            }
     
+            if (randomTmp.every((x) => x.posX !== posRandom.posX || x.posY !== posRandom.posY)) {
+                randomTmp.push(posRandom);
+            }
+        }
+    
+        return randomTmp;
+    }
+
     useEffect( () => {
-       
+        var finalArr = new Array()
+        var numLength : number = data.grid.subGrid.length
+
+        data.grid.subGrid.forEach((element: any, i: number) => {
+            let arrPosGrid : PositionGrid[] = []
+            element.forEach((matrix: any) => {
+                let arrPosToHidden : Position [] = getRandomArbitrary(numLength)
+                let matrixFinal : Cell [][] = []
+                for (let row = 0; row < matrix.cellsMatrix.length; row++) {
+                    matrixFinal[row] = []
+                    for (let column = 0; column < matrix.cellsMatrix.length; column++) {
+                        let numb: number = matrix.cellsMatrix[row][column].number;
+                        let pos: Position = {posX:matrix.cellsMatrix[row][column].row, posY: matrix.cellsMatrix[row][column].column};
+                        let posToHidden : Position[] = arrPosToHidden.filter( posHid => {
+                            return posHid.posX === pos.posX && posHid.posY === pos.posY;
+                        });
+                        if(posToHidden.length !== 0){
+                            var cellTmp : Cell = {num: numb, hidden: true, position: pos}
+                        }else{
+                            var cellTmp : Cell = {num: numb, hidden: false, position: pos}
+                        }
+                        matrixFinal[row][column] = cellTmp
+                    }
+                }
+                let posGrid : PositionGrid = {cellsMatrix: matrixFinal, idGrid: i}
+                arrPosGrid.push(posGrid)
+            });
+            finalArr.push(arrPosGrid)
+        });
+
+        var positionRow = 0
+        var positionCol = 0
+        var matrixFinal : Cell [][] = []
+        var matrixToMatch : Cell [][] | string[][] = []
+        finalArr.map( arrElem => {
+            for (let row = 0; row < numLength; row++) {
+                matrixFinal[positionRow] = []
+                matrixToMatch[positionRow] = []
+                arrElem.map( (valuePosGrid: PositionGrid) => {
+                for (let column = 0; column < valuePosGrid.cellsMatrix.length; column++) {
+                    let cellTmp : Cell = valuePosGrid.cellsMatrix[row][column]
+                    matrixFinal[positionRow][positionCol] = cellTmp
+                    if(valuePosGrid.cellsMatrix[row][column].hidden){
+                        matrixToMatch[positionRow][positionCol] =  "empty"
+                    }else{
+                        matrixToMatch[positionRow][positionCol] = cellTmp
+                    }
+                    positionCol++;
+                }
+                });
+                positionRow++;
+                positionCol = 0;
+            }
+        });
+        console.log(matrixFinal)
+        console.log(matrixToMatch)
     },[data])
 
     function goBackToForm(){
         showForm()
     }
 
-    console.log(data)
     if(table){     
         return(
             <div>
@@ -37,7 +132,7 @@ interface PropGrid{
 }
 
 interface PropGame{
-    table: any
+    grid: any
 }
 
 
@@ -51,10 +146,10 @@ export default function Grid({showForm, grid} : PropGrid){
         getData(grid)
     },[grid])
 
-    async function getData(query: string){
+    function getData(query: string){
         //const response = await fetch ('https://zurdenro-my-app-74i6k.ondigitalocean.app/api/v1/grid/'+ query)
         //const data = await response.json()
-        setData({table : json})
+        setData({grid : json})
         setFetching(false)
     }
 
@@ -62,7 +157,7 @@ export default function Grid({showForm, grid} : PropGrid){
 
     return(
         <>
-           {<GenerateGrid data={data?.table} showForm={showForm}/>}
+           {<GenerateGrid data={data?.grid} showForm={showForm}/>}
         </>
     );
 }
